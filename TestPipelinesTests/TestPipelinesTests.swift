abb71ba440e731afb3689ea9bd0624d2c311a770
Muhammad Usman <m.usmanmazhar@systemsltd.com>
2024-11-08 17:21:25 +0500

    test updated

--- TestPipelinesTests/TestPipelinesTests.swift
+++ TestPipelinesTests/TestPipelinesTests.swift
@@ -10,27 +10,39 @@ import XCTest
 
 final class TestPipelinesTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var viewModel: LoginViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = LoginViewModel()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testUsernameIsEmpty() {
+        XCTAssertFalse(viewModel.validateUsername(userName: ""), "Empty username should be invalid")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testUsernameTooShort() {
+        XCTAssertFalse(viewModel.validateUsername(userName: "ab"), "Username with less than 3 characters should be invalid")
     }
-
+    
+    func testUsernameTooLong() {
+        XCTAssertFalse(viewModel.validateUsername(userName: "abcdefghijklmnopqrstuvwxyz"), "Username with more than 20 characters should be invalid")
+    }
+    
+    func testUsernameWithSpecialCharacters() {
+        XCTAssertFalse(viewModel.validateUsername(userName: "user@name"), "Username with special characters should be invalid")
+    }
+    
+    func testUsernameStartingWithNonLetter() {
+        XCTAssertFalse(viewModel.validateUsername(userName: "1username"), "Username starting with a non-letter should be invalid")
+    }
+    
+    func testValidUsername() {
+        XCTAssertTrue(viewModel.validateUsername(userName: "ValidUser123"), "Username with valid length and alphanumeric characters should be valid")
+    }
 }
