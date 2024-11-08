7c252efcf1f2051b27e16970bdcf2b2f354d904f
Muhammad Usman <m.usmanmazhar@systemsltd.com>
2024-11-08 16:06:06 +0500

    unit tests added + config updated.

---
+++ TestPipelines/Logics/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  TestPipelines
+//
+//  Created by Muhammad Usman on 08/11/2024.
+//
+
+import Foundation
---
+++ TestPipelinesTests/LoginViewModelTest.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModelTest.swift
+//  TestPipelinesTests
+//
+//  Created by Muhammad Usman on 08/11/2024.
+//
+
+import Foundation
